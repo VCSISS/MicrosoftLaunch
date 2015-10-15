222a2a13fd1ba88b9e157d6c70e186840f674b78
M3579 <mihirkasmalkar@gmail.com>
2015-10-14 19:04:32 -0700

    I was unable to push the entire Visual Studio project, so I copied these three files. This is from the web server and client I made to test if two processes can communicate with each other on the Windows 10 IoT on a Raspberry Pi 2

---
+++ client.py
@@ -0,0 +1,29 @@
+﻿import http.client
+import xml.etree.ElementTree as etree
+import _wingpio as pins
+import time
+
+pin = 23
+
+c = http.client.HTTPConnection("", 8000)
+
+try:
+    while True:
+        # request from server (no need to request a url, just make request)
+        c.request("GET", "")
+        response = c.getresponse().read()
+
+        # get number from XML
+        blink = etree.fromstring(response)
+        number = int(blink.text)
+    
+        # flash LED accordingly
+        if number == 1:
+            pins.output(pin, pins.HIGH)
+        else:
+            pins.output(pin, pins.LOW)
+
+        time.sleep(1)
+
+except Exception as e:
+    print(str(e))
