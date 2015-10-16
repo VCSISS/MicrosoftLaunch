02e345811a0992a0a67d13d9d45d08182fac7415
M3579 <mihirkasmalkar@gmail.com>
2015-10-16 14:39:11 -0700

    Added source files of server and client vs projects

--- client/client.py
+++ client/client.py
@@ -5,23 +5,33 @@ import time
 
 pin = 23
 
+print("started")
+
 c = http.client.HTTPConnection("", 8000)
+
+print("got connection")
 
 try:
     while True:
+        print("looping")
         # request from server (no need to request a url, just make request)
         c.request("GET", "")
         response = c.getresponse().read()
+        print("got response")
 
         # get number from XML
         blink = etree.fromstring(response)
         number = int(blink.text)
     
+        print("got number")
+
         # flash LED accordingly
         if number == 1:
             pins.output(pin, pins.HIGH)
         else:
             pins.output(pin, pins.LOW)
+
+        print("flashed LED")
 
         time.sleep(1)
 
