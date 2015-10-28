95bb0ee35ea3b81cac4a882adf54b6a28d5424f1
M3579 <mihirkasmalkar@gmail.com>
2015-10-27 20:48:06 -0700

    Added IoT versions of CommandClient and CommandExecutingServer

---
+++ CommandClientIoT/CommandClientIoT/StartupTask.py
@@ -0,0 +1,29 @@
+﻿import http.client
+
+def status_update(message): print("STATUS:", message)
+
+try:
+    c = http.client.HTTPConnection("127.0.0.1", 8000)
+    
+    status_update("Set up connection with server")
+
+    while True:
+        status_update("Requesting code from server")
+
+        c.request("GET", "")
+
+        response = c.getresponse().read()
+        
+        status_update("Received code from server")
+
+        print("Executing=============================")
+        
+        try:
+            exec(response)
+        except Exception as ex:
+            print(str(ex))
+
+        print("======================================")
+
+except Exception as ex:
+    print(str(ex))
