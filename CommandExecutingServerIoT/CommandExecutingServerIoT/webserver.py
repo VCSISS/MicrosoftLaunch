0a6a1ac22925099292ca3642ed001a4ddafd670c
M3579 <mihirkasmalkar@gmail.com>
2015-10-29 21:36:51 -0700

    Added print functionality (all output goes to client console) to IoT versions of CommandSendingClient and CommandExecutingServer

--- CommandExecutingServerIoT/CommandExecutingServerIoT/webserver.py
+++ CommandExecutingServerIoT/CommandExecutingServerIoT/webserver.py
@@ -1,56 +1,70 @@
-﻿import http.server
-import http.client
-import urllib.parse
-import cgi
-import re
+﻿from __future__ import print_function
+import BaseHTTPServer
+import httplib
+import urlparse
+import urllib
 import traceback
-import _wingpio as pins
 
-c = http.client.HTTPConnection("10.79.1.51", 8001)
+ioc = httplib.HTTPConnection("10.0.0.10", 8001)
 
-def getInput(prompt=""):
-    global c
+def get_input(prompt=""):
+    global ic
 
-    c.request("GET", "",
-                urllib.parse.urlencode({"prompt": prompt}),
-                {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"})
+    ioc.request("GET", "",
+              urllib.urlencode({"prompt": prompt}),
+              {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"})
 
-    response = c.getresponse().read()
+    response = ioc.getresponse().read()
 
     return response.decode("UTF-8")
 
-input = getInput
 
-class CommandExecutingRequestHandler(http.server.BaseHTTPRequestHandler):
+def remote_print(*objects):
+    string = "".join([str(i) for i in objects])
+
+    ioc.request("POST", "",
+                urllib.urlencode({"print": string}),
+                {"Content-type": "application/x-ww-form-urlencoded", "Accept": "text/plain"})
+
+    ioc.getresponse()
+
+input = get_input
+
+
+class CommandExecutingRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(self):
-        self.send_respones(200)
+        self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
     
     def do_POST(self):
-        command = self.path
-
-        self.status_update("Client requested with type POST")
-
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-
-        print("Executing===============================")
-
+        global print
         try:
-            vars = cgi.parse_qs(self.rfile.read(int(self.headers["Content-length"])))
+            self.status_update("Client requested with type POST")
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+
+            old_print = print
+            print = remote_print
+
+            print("Executing===============================")
+
+            vars = urlparse.parse_qs(self.rfile.read(int(self.headers["Content-length"])))
             code = (vars[b"code"])[0]
             codeString = code.decode("UTF-8")
             
             exec(codeString)
 
             self.wfile.write(b"Success")
-        except Exception as ex:
-            traceback.print_exc()
-            self.wfile.write(b"Failure")
 
-        print("========================================")
+        except Exception as ex:
+            traceback.print_exc(file=self.wfile)
+
+        finally:
+            print("========================================")
+            print = old_print
 
     def status_update(self, message):
         print("STATUS:", message)
