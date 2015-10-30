24e5a6126c1754bb8c0595f7556d02c4fb845637
M3579 <mihirkasmalkar@gmail.com>
2015-10-29 21:46:30 -0700

    Modified code to be Python 3

--- CommandExecutingServerIoT/CommandExecutingServerIoT/webserver.py
+++ CommandExecutingServerIoT/CommandExecutingServerIoT/webserver.py
@@ -1,14 +1,14 @@
-﻿from __future__ import print_function
-import BaseHTTPServer
-import httplib
-import urlparse
+﻿import http.server
+import http.client
 import urllib
+import urllib.parse
 import traceback
 
-ioc = httplib.HTTPConnection("10.0.0.10", 8001)
+ioc = http.client.HTTPConnection("10.0.0.10", 8001)
+
 
 def get_input(prompt=""):
-    global ic
+    global ioc
 
     ioc.request("GET", "",
               urllib.urlencode({"prompt": prompt}),
@@ -19,26 +19,25 @@ def get_input(prompt=""):
     return response.decode("UTF-8")
 
 
-def remote_print(*objects):
-    string = "".join([str(i) for i in objects])
+def remote_print(*objects, sep="", end="\n"):
+    string = sep.join([str(i) for i in objects]) + end
 
     ioc.request("POST", "",
                 urllib.urlencode({"print": string}),
-                {"Content-type": "application/x-ww-form-urlencoded", "Accept": "text/plain"})
+                {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"})
 
     ioc.getresponse()
 
 input = get_input
 
 
-class CommandExecutingRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class CommandExecutingRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_HEAD(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
     
     def do_POST(self):
-        global print
         try:
             self.status_update("Client requested with type POST")
 
@@ -51,7 +50,7 @@ class CommandExecutingRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
             print("Executing===============================")
 
-            vars = urlparse.parse_qs(self.rfile.read(int(self.headers["Content-length"])))
+            vars = urllib.parse.parse_qs(self.rfile.read(int(self.headers["Content-length"])))
             code = (vars[b"code"])[0]
             codeString = code.decode("UTF-8")
             
